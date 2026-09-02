3681c86e10b851ea54fb5eb8e042c7813ed04cdb
Franco Ferreyra <ferreyrafranco0802@gmail.com>
2026-09-02 18:13:12 +0200

    Add containerization for Frontend and backend with production support Adjust prisma output path for build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,22 @@
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss()
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const port = Number(env.VITE_PORT) || 5173
+  return {
+    server: {
+      port,
+      strictPort: true,
+      host: true,
+    },
+    plugins: [
+      react(),
+      babel({ presets: [reactCompilerPreset()] }),
+      tailwindcss()
+    ],
+  }
 })
